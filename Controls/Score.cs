7fdeef42f18f4f5f3bf105849f4a160e5b278757
maxton <maxton@maxton.xyz>
2017-02-27 20:26:18 -0500

    Use linear interpolation for the score "now" line

--- Controls/Score.cs
+++ Controls/Score.cs
@@ -16,6 +16,7 @@ namespace Jammit.Controls
     private IReadOnlyList<Beat> _beats;
     private int _beatNum;
     private Track _track;
+    private Pen _line;
 
     protected override void OnResize(EventArgs e)
     {
@@ -32,6 +33,7 @@ namespace Jammit.Controls
       _nodes = nodes.Nodes;
       _beats = beats;
       _track = track;
+      _line = new Pen(Color.FromArgb(0x7F00FF00), 3);
       Invalidate();
     }
 
@@ -48,15 +50,41 @@ namespace Jammit.Controls
       }
     }
 
+    /// <summary>
+    /// Return the x offset to draw.
+    /// </summary>
+    /// <returns></returns>
+    private int Interpolate()
+    {
+      if(_nodes.Count > _beatNum+1)
+      {
+        if (_nodes[_beatNum].Row != _nodes[_beatNum + 1].Row)
+        {
+          var timeDiff = _beats[_beatNum + 1].Time - _beats[_beatNum].Time;
+          var xDiff = _nodes[_beatNum].X - _nodes[_beatNum - 1].X;
+          var timeFromBeat = SamplePosition / 44100.0 - _beats[_beatNum].Time;
+          return (int)(xDiff * timeFromBeat / timeDiff / 1.5);
+        }
+        else
+        {
+          var timeDiff = _beats[_beatNum + 1].Time - _beats[_beatNum].Time;
+          var xDiff = _nodes[_beatNum + 1].X - _nodes[_beatNum].X;
+          var timeFromBeat = SamplePosition/44100.0 - _beats[_beatNum].Time;
+          return (int) (xDiff*timeFromBeat/timeDiff);
+        }
+      }
+      return 0;
+    }
+
     protected override void OnPaint(PaintEventArgs pe)
     {
       if (_imgs != null && _nodes != null && _beats != null)
       {
         UpdateCurrentBeat();
         var xOffset = (Width - _imgs[0].Width)/2;
-        var x = _nodes[_beatNum].X + xOffset;
-        var y = _track.ScoreSystemInterval * _nodes[_beatNum].Row;
-        var img = y/_imgs[0].Height;
+        var x = _nodes[_beatNum].X + xOffset + Interpolate();
+        var y = _nodes[_beatNum].Row == 65535 ? 0 : _track.ScoreSystemInterval * _nodes[_beatNum].Row;
+        var img = _nodes[_beatNum].Row == 65535 ? 0 : y/_imgs[0].Height;
 
         if (_imgs.Count > img)
         {
@@ -64,7 +92,7 @@ namespace Jammit.Controls
           if(_imgs.Count > img + 1)
             pe.Graphics.DrawImageUnscaled(_imgs[img+1], xOffset, -y + _imgs[img].Height + 1024*img);
         }
-        pe.Graphics.DrawLine(Pens.Green, x, 0, x, _track.ScoreSystemHeight);
+        pe.Graphics.DrawLine(_line, x, 0, x, _track.ScoreSystemHeight);
       }
 
       base.OnPaint(pe);
