69ebf02dd185cd85be677d6ae8cb74f1538f6465
Julio C. Rocha <julio@rochsquadron.net>
2017-07-13 03:35:30 -0700

    Mobile - Defined ISongPlayer2 and ISong interfaces.

---
+++ Mobile.Core/Model/ISong.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Jammit.Audio;
+using Xamarin.Forms;
+
+namespace Jammit.Model
+{
+  public interface ISong
+  {
+    SongInfo Metadata { get; }
+
+    IReadOnlyList<Track> Tracks { get; }
+
+    //TODO: Beats
+
+    //TODO: Sections
+
+    sbyte[] GetWaveForm();
+
+    Image GetCover();
+
+    List<Image> GetNotation(Track t);
+
+    List<Image> GetTablature(Track t);
+
+    // GetNotationData
+
+    Stream GetContentStream(string path);
+
+    Stream GetSeekableContentStream(string path);
+
+    ISongPlayer2 GetSongPlayer();
+  }
+}
---
+++ Mobile/Model/PCLStorageSong.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Jammit.Audio;
+using Xamarin.Forms;
+
+namespace Jammit.Model
+{
+  public class PCLStorageSong : ISong
+  {
+    // list scorenodes notationData
+
+    public PCLStorageSong(SongInfo metadata)
+    {
+      Metadata = metadata;
+      Tracks = InitTracks();
+      // Beats
+      // Sections
+      // notationData
+    }
+
+    private List<Track> InitTracks()
+    {
+      throw new NotImplementedException();
+    }
+
+    //InitBeats
+
+    // InitSections
+
+    // InitScoreNodes
+
+    #region ISong members
+
+    public SongInfo Metadata { get; }
+
+    public IReadOnlyList<Track> Tracks { get; }
+
+    public Stream GetContentStream(string path)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Image GetCover()
+    {
+      throw new NotImplementedException();
+    }
+
+    public List<Image> GetNotation(Track t)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Stream GetSeekableContentStream(string path)
+    {
+      throw new NotImplementedException();
+    }
+
+    public ISongPlayer2 GetSongPlayer()
+    {
+      throw new NotImplementedException();
+    }
+
+    public List<Image> GetTablature(Track t)
+    {
+      throw new NotImplementedException();
+    }
+
+    public sbyte[] GetWaveForm()
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+
+  }
+}
