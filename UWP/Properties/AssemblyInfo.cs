656cf0a00bba06bfafa94053a7f1fc7f6eeaa524
Julio C. Rocha <julio@rochsquadron.net>
2018-02-24 03:37:14 -0800

    Renamed non-desktop projects as Unjammit.

--- UWP/Properties/AssemblyInfo.cs
+++ UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jam.NET.UWP")]
+[assembly: AssemblyTitle("Unjammit.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jam.NET")]
+[assembly: AssemblyProduct("Unjammit")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
